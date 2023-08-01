1a7c314f1d928532af06997ef08bb0556c7de30b
Trevor DePew <t.depewmusic@gmail.com>
2023-08-01 16:08:30 -0400

    set print statements to print pieces on board

--- resources/theBoard.py
+++ resources/theBoard.py
@@ -8,8 +8,15 @@ for row in theBoard:
     while i < 8:
         if row[i].piece != None:
             if row[i].coordinate[0] != "h":
-                print(row[i].coordinate, end="|")
+                print(row[i].piece, end="|")
                 i += 1
             else:
-                print(row[i].coordinate)
+                print(row[i].piece)
+                i += 1
+        else:
+            if row[i].coordinate[0] != "h":
+                print("   ", end="|")
+                i += 1
+            else:
+                print("   ")
                 i += 1
