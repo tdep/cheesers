349e07fac6ed1c274b2868d6e123f321cde649fd
Trevor DePew <t.depewmusic@gmail.com>
2023-08-02 09:26:54 -0400

    working on board format

--- resources/theBoard.py
+++ resources/theBoard.py
@@ -1,22 +1,31 @@
 import Square
 
-
 theBoard = Square.theBoard
 
-for row in theBoard:
-    i = 0
-    while i < 8:
-        if row[i].piece != None:
-            if row[i].coordinate[0] != "h":
-                print(row[i].piece, end="|")
-                i += 1
+def printBoard(board):
+    
+    r = 8
+    for row in board:
+        i = 0
+        while i < 8:
+            if row[i].piece != None:
+                if row[i].coordinate[0] != "h":
+                    print(row[i].piece, end=" | ")
+                    i += 1
+                else:
+                    print(row[i].piece, end=" | ")
+                    i += 1
             else:
-                print(row[i].piece)
-                i += 1
-        else:
-            if row[i].coordinate[0] != "h":
-                print("   ", end="|")
-                i += 1
-            else:
-                print("   ")
-                i += 1
+                if row[i].coordinate[0] != "h":
+                    print("   ", end=" | ")
+                    i += 1
+                else:
+                    print("   " + " | " + str(r))
+                    i += 1
+        if r > 1:
+            print(' -  +  -  +  -  +  -  +  -  +  -  +  -  +  -  |')
+        r -= 1
+    print('_____________________________________________ |')
+    print(' A     B     C     D     E     F     G     H')
+
+printBoard(theBoard)
