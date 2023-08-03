6cbe3221cb2fc17c4c57e4e77942545d196d9447
Trevor DePew <t.depewmusic@gmail.com>
2023-08-02 22:42:16 -0400

    adding player class

---
+++ resources/Player.py
@@ -0,0 +1,37 @@
+class Player:
+    def __init__(self, color, pieces, captures):
+        self.color = color
+        self.pieces = pieces
+        self.captures = captures
+        self.check = False
+
+    def checkChecker():
+        # See if king is captured, or if King is only piece left - if so, set check to True
+        pass
+    
+    def colorPicker(choice):
+        # Accept input from player to choose color
+        print("Player 1, please choose a color: Black or White (b/w)?")
+
+        picking = False
+
+        while picking == False:
+            choice = input()
+            if choice == "b":
+                player_1.color = "black"
+                player_2.color = "white"
+                picking = True
+            elif choice == "w":
+                player_1.color = "white"
+                player_2.color = "black"
+                picking = True
+            else:
+                print("Please enter only 'b' or 'w'.")
+                print()
+        print(f"Player 1, you are playing as: {player_1.color}")
+        print(f"Player 2, you are playing as: {player_2.color}")
+
+
+    
+player_1 = Player("", [], [])
+player_2 = Player("", [], [])
